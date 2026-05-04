2779f08428538d5f81b98b6ca6d56bd5711a9129
Maidinh123456 <maidinh823532@gmail.com>
2026-05-05 06:55:14 +0700

    reset repo

--- Backend/repositories/review.repository.js
+++
@@ -1,9 +0,0 @@
-const { Review } = require("../models");
-
-const createReview = async (data) => {
-  return await Review.create(data);
-};
-
-module.exports = {
-  createReview,
-};
--- Backend/routes/review.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ctrl = require("../controllers/review.controller");
-const { verifyToken, checkRole } = require("../middlewares/auth.middleware");
-
-router.post("/", verifyToken, checkRole(["Customer"]), ctrl.createReview);
-
-module.exports = router;
