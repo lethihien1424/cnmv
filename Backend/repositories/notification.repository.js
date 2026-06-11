ff60de3c060ee03bbbc8c29f463ae7c3f66fe311
lethihien1424 <hle26625@gmail.com>
2026-06-12 01:15:58 +0700

    update project

--- Backend/repositories/notification.repository.js
+++
@@ -1,9 +0,0 @@
-const { Notification } = require("../models");
-
-const createNotification = async (payload) => {
-  return Notification.create(payload);
-};
-
-module.exports = {
-  createNotification,
-};
